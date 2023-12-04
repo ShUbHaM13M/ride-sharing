507bc49b15363ef415f40284fc4e2be5ff690513
Shubham Maurya <shubham.heeralal@gmail.com>
2023-12-04 16:07:10 +0530

    Added MATCH command

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from ast import Dict
+import math
+from typing import Dict
 from sys import argv
 import sys
 
+MAX_RANGE = 5
 
 commands = {
     "ADD_DRIVER": ["DRIVER_ID", "X_COORDINATE", "Y_COORDINATE"],
@@ -21,12 +23,63 @@ drivers = {}
 riders = {}
 
 
-def add_driver(info: Dict[str, str | int]):
-    drivers[info["DRIVER_ID"]] = {
-        "x": int(info["X_COORDINATE"]),
-        "y": int(info["Y_COORDINATE"]),
-    }
-    print(f"Added Driver: {info['DRIVER_ID']}")
+def add_driver(info: Dict[str, str]):
+    driver_id = info["DRIVER_ID"]
+    try:
+        drivers[driver_id] = [
+            int(info["X_COORDINATE"]),
+            int(info["Y_COORDINATE"]),
+        ]
+        print(f"Added Driver: {driver_id}")
+    except ValueError:
+        print(f"Error: Coordinate values of Rider {driver_id} is not a number")
+        sys.exit(1)
+
+
+def add_rider(info: Dict[str, str]):
+    rider_id = info["RIDER_ID"]
+    try:
+        riders[rider_id] = [
+            int(info["X_COORDINATE"]),
+            int(info["Y_COORDINATE"]),
+        ]
+        print(f"Added Rider: {rider_id}")
+    except ValueError:
+        print(f"Error: Coordinate values of Rider {rider_id} is not a number")
+        sys.exit(1)
+
+
+def match_rider(info: Dict[str, str]):
+    rider_id = info["RIDER_ID"]
+    rider_pos = riders[rider_id]
+    drivers_in_range = []
+
+    for driver_id, driver_pos in drivers.items():
+        distance = math.floor(math.dist(rider_pos, driver_pos))
+        if distance <= MAX_RANGE:
+            driver = {
+                "id": driver_id,
+                "pos": drivers[driver_id],
+                "distance": distance,
+            }
+            drivers_in_range.append(driver)
+
+    if len(drivers_in_range) == 0:
+        print("NO_DRIVERS_AVAILABLE")
+        return
+
+    drivers_in_range.sort(key=lambda x: x["distance"])
+
+    temp = drivers_in_range[0]["distance"]
+    equidistant = all(val["distance"] == temp for val in drivers_in_range)
+    if equidistant:
+        # TODO: Sort using lexicographical order
+        pass
+
+    print("DRIVERS_MATCHED", end=" ")
+    for driver in drivers_in_range:
+        print(driver["id"], end=" ")
+    print("")
 
 
 def process_command(command: str):
@@ -46,11 +99,9 @@ def process_command(command: str):
         if command == "ADD_DRIVER":
             add_driver(arg_map)
         elif command == "ADD_RIDER":
-            riders[arg_map["RIDER_ID"]] = {
-                "x": int(arg_map["X_COORDINATE"]),
-                "y": int(arg_map["Y_COORDINATE"]),
-            }
-            print(f"Added Rider: {arg_map['RIDER_ID']}")
+            add_rider(arg_map)
+        elif command == "MATCH":
+            match_rider(arg_map)
         else:
             print("Error: Command Not Yet Implemented", file=sys.stderr)
 
